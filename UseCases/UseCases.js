f208977c0b850a77d35b5de77410de506d71367d
W3nq0 <165017532+W3nq0@users.noreply.github.com>
2024-04-11 10:29:38 +0300

    Update UseCases.js

--- UseCases/UseCases.js
+++ UseCases/UseCases.js
@@ -1,13 +1,18 @@
-
+// UseCases/index.js
 const FileManagerGetDiff = require('./FileManager/getDiff');
 const FileManagerGetStreet = require('./FileManager/getStreet');
 const GeneratorExcel = require('./StreetService/GeneratorExcel');
+const AddressService = require('./StreetsManager/getApi');
 
 class UseCases {
     constructor() {
         this.fileManagerDiff = new FileManagerGetDiff();
         this.fileManagerStreet = new FileManagerGetStreet();
         this.generatorExcel = new GeneratorExcel();
+        this.checkAddressesForStreet = new AddressService();
+        this.maxHouseNumber = 1000;
+        this.maxCorpusNumber = 2000;
+        this.streets = [];
     }
 
     async getStreets() {
@@ -20,6 +25,9 @@ class UseCases {
 
     async generateExcel() {
         return await GeneratorExcel.generate();
+    }
+    async checkAddressesForStreet() {
+        return await AddressService.checkAddressesForStreet()
     }
 }
 
