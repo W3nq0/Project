0275d4d4c1e7b5a8654b074d419162e1ad8d9fc9
W3nq0 <165017532+W3nq0@users.noreply.github.com>
2024-04-11 10:30:50 +0300

    Update index.js

--- Delivery/http/index.js
+++ Delivery/http/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const StreetRoutes = require('./StreetRoutes');
 const FileManagerRoutes = require('./FileManagerRoutes');
+const AddressRoutes = require('./AddressRoutes');
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,8 @@ app.use(bodyParser.json());
 
 app.use('/streets', StreetRoutes);
 app.use('/files', FileManagerRoutes);
+app.use('/address', AddressRoutes);
+
 
 app.listen(port, () => {
     console.log(`Сервер запущен на порту ${port}`);
