a83d3d2ba55503ff0cb99704fcea04b508fd38fc
W3nq0 <165017532+W3nq0@users.noreply.github.com>
2024-04-17 10:17:33 +0300

    Update index.js

--- Delivery/http/index.js
+++ Delivery/http/index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const StreetRoutes = require('./StreetRoutes');
-const FileManagerRoutes = require('./FileManagerRoutes');
-const AddressRoutes = require('./AddressRoutes');
+const AllStreets = require('./allStreet');
+const FileManager = require('./FileManager');
+const CacheAddress = require('./checkAddress');
+const createUseCases = require('../../UseCases/UseCases.js');
+const path = require('path'); 
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-app.use('/streets', StreetRoutes);
-app.use('/files', FileManagerRoutes);
-app.use('/address', AddressRoutes);
+const filePath = path.resolve(__dirname, '../../Data/AD.json');
 
+const useCases = createUseCases(filePath);
+
+app.use('/streets', AllStreets(useCases));
+app.use('/files', FileManager(useCases));
+app.use('/address', CacheAddress(useCases));
 
 app.listen(port, () => {
     console.log(`Сервер запущен на порту ${port}`);
 });
-
-module.exports = app;
